d7d433ab1340587042495626bfa786f17bf69504
asonas <hzw1258@gmail.com>
2012-06-04 01:10:50 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YayoiFes::Application.config.secret_token = '24aaae42b7111b3c251ae3d39baa65523fdb93a61f468b2b4ba00579d84cc22cc320b3d58859703e2ada14ae3301608f99a8201703ece8b7fbe7f90628b8c0f1'
